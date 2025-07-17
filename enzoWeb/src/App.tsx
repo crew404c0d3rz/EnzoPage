e35ba69f998497c7881302a8fa561735417abf5f
Enzo Ibarra <ibarraenzo139002@gmail.com>
2025-07-17 15:15:04 -0300

    Responsive y ajustes

--- enzoWeb/src/App.tsx
+++ enzoWeb/src/App.tsx
@@ -12,21 +12,24 @@ function App() {
   return (
     <>
    <div id='home'><Header/></div>
-   <div className='principal'>
-  <div className='imgDev'><img src="../public/img/icoDev.png" alt="" /></div>
-  <div className="texto-superpuesto">
-    <h1>ENZO IBARRA</h1>
-    <p>Desarrollador Full-Stack y diseñador web que crea experiencias digitales con pasión y precisión</p>
-    <div className='containButton'>
-  <a href="#proyectos">
-    <button id='job'>Ver mis trabajos</button>
-  </a>
-  <a href="#contacto">
-    <button id='contact'>Ponte en contacto</button>
-  </a>
-</div>
+ <div className='principal'>
+  <div className="hero-container">
+    <div className="hero-content">
+      <h1>ENZO IBARRA</h1>
+      <p>Desarrollador Full-Stack y diseñador web que crea experiencias digitales con pasión y precisión.</p>
+      <div className='containButton'>
+        <a href="#proyectos">
+          <button id='job'>Ver mis trabajos</button>
+        </a>
+        <a href="#contacto">
+          <button id='contact'>Ponte en contacto</button>
+        </a>
+      </div>
+    </div>
+    
   </div>
 </div>
+
    <div id='sobreMi'><SobreMi/></div>
  <div id='proyectos'><Proyectos/></div> 
    <div id='contacto'><Contacto/></div>
